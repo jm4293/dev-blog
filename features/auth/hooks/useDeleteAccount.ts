a9b3ca541e0163864283678a2a8d0d46d9c67caa
jm4293 <dlwoals4293@gmail.com>
2026-01-14 23:44:00 +0900

    fix: 로그아웃, 탈퇴 후 query 상태가 유지되었던 오류 수정

--- features/auth/hooks/useDeleteAccount.ts
+++ features/auth/hooks/useDeleteAccount.ts
@@ -1,17 +1,20 @@
 'use client';
 
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { deleteAccountAction } from '@/features/auth/actions/deleteAccount.action';
 import { useRouter } from 'next/navigation';
 
 export const useDeleteAccount = () => {
   const router = useRouter();
+  const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: async () => {
       await deleteAccountAction();
     },
     onSuccess: () => {
+      // React Query 캐시 초기화
+      queryClient.clear();
       // 회원탈퇴 완료 후 메인 페이지로 이동
       router.push('/posts');
     },
--- features/auth/hooks/useLogout.ts
+++ features/auth/hooks/useLogout.ts
@@ -1,17 +1,20 @@
 'use client';
 
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { logoutAction } from '@/features/auth/actions/logout.action';
 import { useRouter } from 'next/navigation';
 
 export const useLogout = () => {
   const router = useRouter();
+  const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: async () => {
       await logoutAction();
     },
     onSuccess: () => {
+      // React Query 캐시 초기화
+      queryClient.clear();
       router.push('/posts');
     },
     onError: (error) => {
