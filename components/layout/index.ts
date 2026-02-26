57fe69cd7e095f198370701b040e0ed47202bee2
jm4293 <dlwoals4293@gmail.com>
2026-02-27 01:05:04 +0900

    feat: 로그인 필요 메뉴에 안내 텍스트 추가
    - 데스크탑 사이드바와 모바일 메뉴에서 즐겨찾기/프로필 항목에 "(로그인 필요)" 표시

--- components/layout/index.ts
+++ components/layout/index.ts
@@ -1,6 +1,6 @@
+export * from './desktop-sidebar';
 export * from './footer-container';
 export * from './layout-container';
 export * from './mobile-hamburger';
 export * from './mobile-header';
 export * from './mobile-menu';
-export * from './desktop-sidebar';
